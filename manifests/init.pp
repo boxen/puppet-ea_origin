599d7b0c805b3429f8679dde76a5d557472cbdcd
Joey van Dijk <opensource@techtribe.nl>
2013-07-03 15:24:07 +0200

    indentation fixed.

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,8 +4,8 @@
 #
 #   include origin
 class ea_origin {
-  package { 'EA Origin':
-    provider => 'appdmg_eula',
-    source => 'https://eaassets-a.akamaihd.net/Origin-Client-Download/origin/mac/Origin.dmg'
-  }
+    package { 'EA Origin':
+      provider => 'appdmg_eula',
+      source   => 'https://eaassets-a.akamaihd.net/Origin-Client-Download/origin/mac/Origin.dmg'
+    }
 }
